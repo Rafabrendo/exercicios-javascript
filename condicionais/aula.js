0c7dcf32b34aac9db5a366dd86bf748e47776a72
Rafael Brendo <rafalive122@hotmail.com>
2023-10-02 14:27:24 -0300

    Introdução a condicionais

---
+++ condicionais/aula.js
@@ -0,0 +1,12 @@
+// const camisetaCorAzul = true;
+// const camisetaDeOutraCor = false;
+
+let numero = 10;
+const eNumeroPar = numero % 2 == 0 ? true : false;
+//condição ternaria
+
+let num = 11;
+const numeroPar = (num % 2) === 0;
+
+console.log(eNumeroPar)
+console.log(numeroPar)
