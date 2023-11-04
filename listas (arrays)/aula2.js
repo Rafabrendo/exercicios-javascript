542e50507de92b5655c19ccb491739249de3e540
Rafael Brendo <rafalive122@hotmail.com>
2023-11-04 16:14:13 -0300

    Utilizando for

--- listas (arrays)/aula2.js
+++ listas (arrays)/aula2.js
@@ -12,3 +12,11 @@ console.log(notas.length)
 const media = notas.reduce((acumulador, elemento) => acumulador+elemento, 0) / notas.length ;//0 é o valor inicial do acumulador.
 
 console.log(media)
+
+//Lembre-se, tem que colocar var ou let, que são declarações de variaveis
+var soma = 0;
+//vou utilizar o for 
+for (let i = 0; i < notas.length; i++) {
+    soma += notas[i];
+}
+console.log(soma)
---
+++ listas (arrays)/aula3.js
@@ -0,0 +1,13 @@
+const nome = "Rafael Brendo Costa dos Reis";
+
+console.log(nome.length)
+
+
+var nome2 = '';
+for (let i = nome.length-1; i > 0; i--) {
+    nome2 += nome[i];
+    
+}
+
+console.log(nome)
+console.log(nome2)
