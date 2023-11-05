e29e13bfcc2e4ea95801a4914cda8ebf9fd5d70f
Rafael Brendo <rafalive122@hotmail.com>
2023-11-05 20:58:36 -0300

    testes

--- listas (arrays)/aula2.js
+++ listas (arrays)/aula2.js
@@ -9,9 +9,10 @@ notas.push(5);
 
 console.log(notas.length)
 
-const media = notas.reduce((acumulador, elemento) => acumulador+elemento, 0) / notas.length ;//0 é o valor inicial do acumulador.
-
-console.log(media)
+function media(arrays){
+    return (arrays.reduce((acumulador, elemento) => acumulador+elemento, 0) /  notas.length).toFixed(2);//0 é o valor inicial do acumulador.
+}
+// console.log(media)
 
 //Lembre-se, tem que colocar var ou let, que são declarações de variaveis
 var soma = 0;
@@ -20,3 +21,9 @@ for (let i = 0; i < notas.length; i++) {
     soma += notas[i];
 }
 console.log(soma)
+
+notas.push(7)
+
+console.log(notas)
+console.log(notas.reduce((acumulador, elemento) => acumulador+elemento, 0))
+console.log(media(notas))
