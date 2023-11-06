9af01a9bd6178f0159b677893f8d06dd313632ad
Rafael Brendo <rafalive122@hotmail.com>
2023-11-06 12:58:38 -0300

    atualização do execicio tabuada.

--- listas (arrays)/exe01.js
+++ listas (arrays)/exe01.js
@@ -2,11 +2,16 @@
 
 
 
-
 function tabuada(n){
     numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    return numeros.map((x) => x * n);
+    let a = numeros.map((x) => x * n);
+    for (let i = 0; i < a.length; i++) {
+        console.log(numeros[i]+ " * " + n+"= "+a[i]);
+    }
 }
 
+
+
 console.log(tabuada(2))
+console.log()
 console.log(tabuada(3))
