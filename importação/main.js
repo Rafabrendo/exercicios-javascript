eb23d543251dac82907f2efd185c958a719ff169
Rafael Brendo <rafalive122@hotmail.com>
2023-11-07 16:06:02 -0300

    Destructuring assignment

--- importação/main.js
+++ importação/main.js
@@ -1,6 +1,28 @@
+//Tô usando a função require para importar o objeto
+// const funcoes = require('./aula');
 
-const funcoes = require('./aula');
+//com o uso do Destructuring
+const {get, print} = require('./aula');
 
-console.log(funcoes)
 
-console.log(funcoes.get());
+// console.log(funcoes)
+
+// console.log(funcoes.get());
+
+//Exemplo de Object Destructuring
+
+const pessoa = {
+    nome: 'Rafa'
+}
+
+//Passei o atributo nome: 'Rafa' de pessoa para a constante nome
+//const { nome } = pessoa;
+
+//console.log(nome)
+
+const nome = pessoa.nome; //É a mesma coisa do código de cima
+console.log(nome)
+
+print(get());
+
+console.log(get())
