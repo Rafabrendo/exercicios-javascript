09bfd79483e2c44f5174e1c27b2c3c5c8621eaf5
Rafael Brendo <rafalive122@hotmail.com>
2023-11-08 19:53:36 -0300

    Inicio da questão

---
+++ importação/exercicio01.js
@@ -0,0 +1,5 @@
+/*
+Uma sala contem 5 alunos e para cada aluno foi sorteado um numero de 1 - 100.
+Faça um programa que receba os 5 números sorteados para os alunos e mostre o maiuor número sorteado. */
+
+var alunos = ['aluno1', 'aluno2', 'aluno3', 'aluno4'] 
