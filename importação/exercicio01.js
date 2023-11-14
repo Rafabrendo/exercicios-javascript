309a646507686b2e287494e15d90cdb5f8d9806a
Rafael Brendo <rafalive122@hotmail.com>
2023-11-14 18:52:39 -0300

    Mostrando o maior valor de um numero sorteado

--- importação/exercicio01.js
+++ importação/exercicio01.js
@@ -2,4 +2,24 @@
 Uma sala contem 5 alunos e para cada aluno foi sorteado um numero de 1 - 100.
 Faça um programa que receba os 5 números sorteados para os alunos e mostre o maiuor número sorteado. */
 
-var alunos = ['aluno1', 'aluno2', 'aluno3', 'aluno4'] 
+var alunos = {
+    0: '',
+    1: '',
+    2: '',
+    3: ''};
+
+var tamanhoDict = Object.keys(alunos).length;
+
+for (let i = 0; i < tamanhoDict; i++) {
+    let variavel = Math.floor(Math.random() * 100);
+    alunos[i] = variavel
+        
+}
+
+var maiorValor = parseInt(Object.values(alunos));
+
+var maiorValor2 = Math.max(maiorValor)
+
+console.log(alunos, maiorValor2)
+
+
