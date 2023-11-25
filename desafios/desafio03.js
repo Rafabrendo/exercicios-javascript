baea56a96de9a23b51c7f89de38961f403503f43
Rafael Brendo <rafalive122@hotmail.com>
2023-11-25 13:20:52 -0300

    Utilizando o .includes()

---
+++ desafios/desafio03.js
@@ -0,0 +1,18 @@
+const totalSalas = 2
+
+const salasComTesouro = [2, 4, 7];
+const salasComMonstro = [3, 6, 8];
+
+//Aqui temos uma forma de loop para percorrer cada sala da masmorra:
+for (let sala = 1; sala <= totalSalas; sala++) {
+//Utilizamos o .includes() para verificar se um número está presente nos arrays:
+    const temTesouro = salasComTesouro.includes(sala);
+    const temMonstro = salasComMonstro.includes(sala);
+
+//TODO: Agora implemente uma condição necessária para verificar e exibir se há tesouro na sala e se há montro na sala:
+    if (temTesouro ) {
+        console.log("Tesouro na sala " + sala + "!");
+    } else if ( temMonstro ) {
+        console.log("Monstro na sala " + sala + "!");
+    }
+}
