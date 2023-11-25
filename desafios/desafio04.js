270c662ddfce20dd7a62c4a4e45eaff53d593ef3
Rafael Brendo <rafalive122@hotmail.com>
2023-11-25 14:03:08 -0300

    update!

--- desafios/desafio04.js
+++ desafios/desafio04.js
@@ -1,4 +1,4 @@
-const quantidadeGolpes = 4;
+const quantidadeGolpes = 2;
 
 // TODO: Defina aqui os tipos de minerais Carvao, Ferro, Diamante e Pedra
 let minerais = ["Carvao", "Ferro", "Diamante", "Pedra"];
@@ -6,8 +6,8 @@ let minerais = ["Carvao", "Ferro", "Diamante", "Pedra"];
 // Loop para cada golpe, de 1 até a quantidade informada
 for (let i = 1; i <= quantidadeGolpes; i++) {
   // Calcula o índice do mineral usando o operador de módulo (%) para garantir que o índice esteja dentro do tamanho do array
-  let minaIndex = i % minerais.length;
+  let minaIndex = (i-1) % minerais.length;
   console.log(minaIndex)
   // TODO: Agora exiba o índice i, concatene com o caractere ":", após, concatene com tipo de minerais[minaIndex]:
-  console.log(i + ": " + minerais[i-1]);
+  console.log(i + ": " + minerais[minaIndex]);
 }
