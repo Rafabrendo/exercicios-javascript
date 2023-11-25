cf6f9d029615ff285fa6711d9c1b6f9fda4e9c93
Rafael Brendo <rafalive122@hotmail.com>
2023-11-25 13:03:10 -0300

    desafio dio 02

---
+++ desafios/desafio02.js
@@ -0,0 +1,9 @@
+/*Escreva um programa simples que simule o ganho de XP após derrotar um monstro. O programa deve calcular e exibir a quantidade de XP ganhos com base no nível do monstro e na dificuldade da batalha.
+ 
+ Para calcular a quantidade de XP ganhos, o programa precisa considerar o nível do monstro e a dificuldade da batalha. A fórmula num1 * num2 * 100 é usada para calcular essa quantidade com base nos valores fornecidos.
+ */
+let num1 = 15;
+let num2 = 20;
+
+let xp = num1 * num2 * 100;
+console.log(xp)
