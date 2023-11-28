84fd7fca70dce04d6b27a6828183cc6ea65aee36
Rafael Brendo <rafalive122@hotmail.com>
2023-11-27 22:02:54 -0300

    Atualizaação

--- variaveisEEscopo/aula01.js
+++ variaveisEEscopo/aula01.js
@@ -6,3 +6,15 @@
  var var1 = 10;
  var1 = 'texto'
  var var2 = 'Teste';
+
+ console.log(var1 + var2)
+
+ var1 = 20;
+ console.log(var1 + var2);
+
+ var2 = '20';
+ console.log(var1 + var2)
+ //Utilizando o operador + ,o interpretador entende que é uma concatenação
+
+ console.log(var1 - var2)
+ //Utilizando o operador -, o interpretador entende que é uma operação matematica
