e0c096f3cc197f7334f049df27f0c7427ebd6e60
Rafael Brendo <rafalive122@hotmail.com>
2023-11-29 11:07:50 -0300

    Mais exemplos de Hoisting

--- variaveisEEscopo/aula02.js
+++ variaveisEEscopo/aula02.js
@@ -5,3 +5,23 @@ teste();
 function teste(){
     console.log('teste');
 }
+
+console.log(var1)
+//Apenas a variavel por elevada, sem a atribuição. E por isso dá undefined.
+var var1 = 'rafa'; 
+
+console.log(var1)
+
+teste2();
+
+//Nesse caso o hoisting vai funcionar de forma diferente. Ele vai primeiro manda a variavel pro topo, e não a atribuição. Por isso, se eu chamar essa variavel antes da declaração, vai dar erro.
+var teste1 = function(){
+    console.log('teste1');
+}
+
+//Nesse caso ele, interpretador, elevou a função, e por isso ela pode ser chamada antes da sua declaração
+function teste2(){
+    console.log('teste2')
+}
+
+teste1();
