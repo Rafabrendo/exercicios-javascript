40b93efa516b473639e729c8752f94f512fac5b3
Rafael Brendo <rafalive122@hotmail.com>
2023-11-29 12:23:58 -0300

    convenções de nomeclaturas em JS

---
+++ variaveisEEscopo/aula04.js
@@ -0,0 +1,8 @@
+//Convenções de nomeclaturas
+//cameCase -> Para nomes de variáveis e funções em JS
+//PascalCase -> É frequentemente usado para nomes de classes ou construtores em JS.
+
+//declarando uma constante
+const TESTE = 10;
+
+const testeVariavel = 10;
