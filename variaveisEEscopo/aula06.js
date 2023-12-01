153c4f5273da4891dd673714605a4ca8538b1b96
Rafael Brendo <rafalive122@hotmail.com>
2023-11-30 21:29:21 -0300

    Começo da aula 06

---
+++ variaveisEEscopo/aula06.js
@@ -0,0 +1,24 @@
+/*  
+    Tipos de dados:
+
+    boolean
+    null
+    undefined
+    -number
+    -string
+    -synbol
+ 
+ */
+
+//object
+
+// +Infinity -Infinity e NaN
+// Double Precision 64-bit binary format IEEE 754
+//obs.: O padrão acima pode ter falhas de arredondamento
+//Se precisar de precisões melhores, utilize o Decimal
+
+const x = {nome: null};
+
+console.log(JSON.stringify(x))
+
+
