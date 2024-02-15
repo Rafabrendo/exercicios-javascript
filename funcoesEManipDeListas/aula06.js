7bbbb00360cd8bcc985ec42708deeac7e6bf96e1
Rafael Brendo <rafalive122@hotmail.com>
2024-02-15 12:12:59 -0300

    filter

--- funcoesEManipDeListas/aula06.js
+++ funcoesEManipDeListas/aula06.js
@@ -17,4 +17,20 @@ lista.forEach((value, i, listRef) => {
 })
 
 
-lista.forEach(cb)
+lista.forEach(cb) //Nesse caso, não se tem uma função anonima e sim uma função que já foi declarada.
+
+//forEach() é um dos métodos mais comuns disponíveis para iterar sobre elementos de um array em JS. Ele executa uma função de callback uma vez para cada elemento no array, em ordem, e aceita três argumentos: o elemento atual, p índice do elemento e o próprio array. No entanto, você geralmente só precisa do primeiro parâmetro, o elemento atual.
+
+console.log()
+
+const frutas = ["Maça", "Banana", "Morango", "Uva"]
+
+frutas.forEach(function(fruta){
+    console.log(fruta);
+});
+//Esse código ira iterar sobre cada elemento do array 'frutas' e imprimir cada elemento no console.
+
+//utilizando arrow functions
+frutas.forEach(fruta => console.log(fruta))
+
+//explicação: tem-se uma função anonima, no caso 'fruta' e cada elemento do array, que está sendo iterado, é passado para essa função anonima e consequentemente retornado.
---
+++ funcoesEManipDeListas/filter.js
@@ -0,0 +1,24 @@
+/*A filtragem geralmente se refere ao processo de selecionar determinados elementos de uma matriz com base em critérios específicos. Isso pode ser alcançado usando o método 'filter()', disponível para arrays.*/ 
+
+//predicate: (value: number, index: number, array: number[]) => value is number, thisArg?: any): number[] (+1 overload)
+
+const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const listaDeNumerosPares = lista.filter((element, i, listRef) => {
+    return (element % 2 === 0)
+}) //Estou criando uma nova lista, e isso difere do forEach porque o forEach só percorre.
+
+console.log(listaDeNumerosPares)
+
+//outro jeito
+
+const numeros = [1,2,3,4,5,6];
+
+const numerosPares = numeros.filter(function(numero){
+    return numero % 2 === 0;
+});
+
+console.log(numerosPares)
+
+
+
