4efb51abf3889f8f40dbe87b83714bf0941e328d
Rafael Brendo <rafalive122@hotmail.com>
2024-04-02 16:07:26 -0300

    atualização desafio02

--- avaliacoes/desafio02/exe01.js
+++ avaliacoes/desafio02/exe01.js
@@ -2,3 +2,18 @@
 2) Faça um programa que receba N (quantidade de númeroos) e seus respectivos valores
 Imprima o maior número par e o menor número ímpar.
  */
+
+const {maiorNumeroPar2, numImpar,print} = require('./funcoes-auxiliares');
+lista = [5,7,8,9,10,14,16]
+
+// console.log(maiorNumeroPar2(lista));
+
+const numPar = maiorNumeroPar2;
+const numImp = numImpar;
+
+
+// print(numPar(lista))
+
+print(numImp(lista))
+
+
--- avaliacoes/desafio02/funcoes-auxiliares.js
+++ avaliacoes/desafio02/funcoes-auxiliares.js
@@ -7,8 +7,9 @@ Imprima o maior número par e o menor número ímpar.
     console.log(texto)
  }
 
- function maiorNumeroPar(lista){
-    let maiorNumPar = Math.max(...lista); //Um jeito de fazer
+//Fiz funções que imprimem o maior valor 
+ function maiorNumero(lista){
+    let maiorNum = Math.max(...lista); //Um jeito de fazer
     let num = lista[0];
     
     // for(let i = 0; i < lista.length; i++){
@@ -23,7 +24,7 @@ Imprima o maior número par e o menor número ímpar.
     //     console.log(lista[i])
     // }
     
-    for(let i = 1; i < lista.length; i++){
+    for(let i = 0; i < lista.length; i++){
         
         if(lista[i] > num ){
             num = lista[i];
@@ -36,6 +37,53 @@ Imprima o maior número par e o menor número ímpar.
     return ` e ${num}`;
  }
 
- var lista = [20,30,0,5,7,8]
+ var lista = [20,30,0,5,7,8,54,61]
 
- console.log(maiorNumeroPar(lista))
+ console.log(maiorNumero(lista))
+
+//  fazendo com reduce
+
+var max = lista.reduce(function(a, b){
+    return Math.max(a, b);
+}, -Infinity)
+// O valor de -Infinity é usado como valor inicial porque qualquer número na array será maior do que -Infinity. Isso garante que o primeiro elemento da array seja comparado com os outros elementos, garantindo que o maior valor seja corretamente identificado.
+console.log(max)
+
+function maiorNumeroPar(lista){
+    let numPar = '';
+    
+    for(let i = 0; i < lista.length; i++){
+    
+     if(lista[i] % 2 === 0){
+        if(numPar === null){
+            numPar = lista[i];
+        }
+        if(lista[i] > numPar){
+            numPar = lista[i]
+        }
+    }
+    
+    }
+
+    return numPar;
+
+}
+
+function maiorNumeroPar2(lista){
+    return lista.filter((a) => a % 2 === 0).reduce(function(a, b){
+        return Math.max(a, b);
+    }, -Infinity)
+}
+
+// console.log(maiorNumeroPar2(lista))
+
+console.log(maiorNumeroPar(lista))
+
+//Elegante
+function numImpar(lista){
+    return lista.filter((a) => a % 2 != 0).reduce((a, b) => Math.min(a, b),);
+}
+
+// console.log(numImpar(lista))
+
+module.exports = {maiorNumeroPar2, numImpar, print};
